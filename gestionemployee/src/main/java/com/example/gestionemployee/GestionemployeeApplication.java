1be9fb66632512d3a4ac3965823f3de934a2a3e3
melek <malekgomri881@gmail.com>
2024-08-25 12:09:54 +0200

    instalation environnement

---
+++ gestionemployee/src/main/java/com/example/gestionemployee/GestionemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionemployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionemployeeApplication.class, args);
+    }
+
+}
