448f8b8a04b4b0752c483708a7e0304dc6ab39bf
melek <malekgomri881@gmail.com>
2024-09-10 22:38:43 +0200

    ajout de quelque page

---
+++ employeereact/src/Home.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+class Home extends React.Component{
+    render(){
+        return <div>
+            welcome to home page and terminale
+        </div>
+    }
+}
+export default Home
