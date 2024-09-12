28afb9188303fc8732c15e58bc7c89a9abe9ef7a
melek <malekgomri881@gmail.com>
2024-09-12 20:59:53 +0200

    forms and button

--- employeereact/src/App.js
+++ employeereact/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import {x,name} from'./Test';
 
 function App() {
+ 
   return (
     <div className="App">
-     <h1>Hello world</h1>
+      
+     <h1 style={{color:"blue",fontSize:"100px"}}>Hello world</h1>
+     <h1>{x} and {name}</h1>
      <Home/>
     </div>
   );
--- employeereact/src/Home.js
+++ employeereact/src/Home.js
@@ -1,8 +1,20 @@
 import React,{Component} from 'react';
 class Home extends React.Component{
+    handlename =(e)=>{
+        console.log(e.target.value)
+      }
+      handletextearea=(e)=>{
+        console.log(e.target.value)
+      }
     render(){
         return <div>
-            welcome to home page and terminale
+            <input type="text" placeholder="name" onChange={this.handlename}></input><br></br>
+            welcome to home page and terminale<br></br>
+            <textarea onChange={this.handletextearea}></textarea><br></br>
+            Men :<input type="radio" name="b" value="men"></input>
+            women:<input type="radio" name="b" value="women"></input><br></br>
+            Tunisia :<input type="checkbox" name="b2" value="tunisia"></input>
+            Algeria:<input type="checkbox" name="br" value="algeria"></input>
         </div>
     }
 }
