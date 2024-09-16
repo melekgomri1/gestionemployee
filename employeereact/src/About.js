ffd052e6334fcd9fef24fd31ade23245cfd62af4
melek <malekgomri881@hotmail.com>
2024-09-16 21:57:19 +0200

    route home and about

---
+++ employeereact/src/About.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class About extends React.Component{
+   
+    render(){
+        return <div>
+            <h1>hello fabiano caruana</h1>
+           
+        </div>
+    }
+}
+export default About
