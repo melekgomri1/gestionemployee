e5d4fb3061485b90250b5f17ee0a6f0fe01d2c51
melek <malekgomri881@hotmail.com>
2024-09-18 21:43:20 +0200

    cc

--- employeereact/src/App.js
+++ employeereact/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './Home';
 import About from './About';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Samia from './Samia';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -10,10 +12,14 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/samia">Samiaa</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/samia" element={<Samia />} />
+          <Route path="/contact/:name/:id" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ employeereact/src/Contact.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class Contact extends React.Component{
+   
+    render(){
+        return <div>
+            <h1>samia w kamel l mba3bsin</h1>
+           
+        </div>
+    }
+}
+export default Contact
