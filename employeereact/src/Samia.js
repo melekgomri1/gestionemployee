ee9543f4438f88d1116429bcbc4799b646f1b3c5
melek <malekgomri881@hotmail.com>
2024-09-21 14:08:42 +0200

    react state and props

--- employeereact/src/Samia.js
+++ employeereact/src/Samia.js
@@ -1,13 +1,21 @@
 import React,{Component} from 'react';
+import Kamel from './Kamel';
 class Samia extends React.Component{
+    state={
+        name:"melek",
+        lastename:"gomri",
+        age:12
+    }
    
     render(){
         console.log(this.props);
         return <div>
+            {this.state.name}<br></br>
+            {this.state.lastename}<br></br>
+            {this.state.age}
             
             <h1>Samia tajourya hhhh</h1>
-           
-           
+            <Kamel data={this.state}/>  
         </div>
     }
 }
