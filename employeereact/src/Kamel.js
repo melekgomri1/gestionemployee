67ba0c0f7e2859c21d6d99c33acda0bacd0daa15
melek <malekgomri881@hotmail.com>
2024-09-28 11:50:02 +0200

    usestate

--- employeereact/src/Kamel.js
+++ employeereact/src/Kamel.js
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 function Kamel(props) {  // Add 'props' as a parameter
     const navigate = useNavigate();
-   
+   const [count,setCount]=React.useState(0);
+   const [firstname,setfirstname]=React.useState("melek tekup")
 
     const handleClick = () => {
         navigate('/about');
@@ -14,6 +15,11 @@ function Kamel(props) {  // Add 'props' as a parameter
         <div>
           {/* Access props directly */}
             <h1>Kamel Gomriaa</h1>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button>
+            <h1>My first name:{firstname}</h1>
+            <button onClick={()=>setfirstname("boussidi w idi fi zebi")}> change name</button>
             
             <button onClick={handleClick}>click</button><br />
         </div>
