98607deb9c241227250d169ff8e042296e644f55
melek <malekgomri881@hotmail.com>
2024-09-28 12:18:50 +0200

    useEffet

--- employeereact/src/Kamel.js
+++ employeereact/src/Kamel.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Kamel(props) {  // Add 'props' as a parameter
+function Kamel(props) {
     const navigate = useNavigate();
-   const [count,setCount]=React.useState(0);
-   const [firstname,setfirstname]=React.useState("melek tekup")
+    const [count, setCount] = React.useState(0);
+    const [firstname, setFirstname] = React.useState("Melek Tekup");
 
     const handleClick = () => {
         navigate('/about');
     };
-   
+
+    useEffect(() => {
+        console.log("welcome home");
+    }); 
+    useEffect(() => {
+        console.log("welcome tek-up");
+    });
 
     return (
         <div>
-          {/* Access props directly */}
+            {/* Access props if needed */}
             <h1>Kamel Gomriaa</h1>
             <h1>{count}</h1>
-            <button onClick={()=>setCount(count+1)}>Increment</button>
-            <button onClick={()=>setCount(count-1)}>Decrement</button>
-            <h1>My first name:{firstname}</h1>
-            <button onClick={()=>setfirstname("boussidi w idi fi zebi")}> change name</button>
-            
-            <button onClick={handleClick}>click</button><br />
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
+            <h1>My first name: {firstname}</h1>
+            <button onClick={() => setFirstname("Boussidi w idi fi zebi")}>Change name</button>
+            <button onClick={handleClick}>Click</button><br />
         </div>
     );
 }
